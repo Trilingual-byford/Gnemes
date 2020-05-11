8dd8dd6570006a6c1033e5468a03ad1e4125670d
niconicocbf <niconicocbf@icloud.com>
2020-05-11 17:31:19 +0900

    set up android app

---
+++ GnemesAndroid/app/src/main/java/com/malygos/gnemes/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.malygos.gnemes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
