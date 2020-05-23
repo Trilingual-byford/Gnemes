51c2a13161be4b32c1da1b8278212d23aeddcb3f
niconicocbf <niconicocbf@icloud.com>
2020-05-23 15:29:13 +0900

    Internet offLineMode implemented

--- GnemesAndroid/app/src/main/java/com/malygos/gnemes/ui/fragment/post/NewPostFragment.kt
+++ GnemesAndroid/app/src/main/java/com/malygos/gnemes/ui/fragment/post/NewPostFragment.kt
@@ -5,7 +5,6 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -16,44 +15,46 @@ import com.malygos.gnemes.data.network.GnemesApiService
 import com.malygos.gnemes.data.repository.MemePostRepository
 import com.malygos.gnemes.databinding.NewPostFragmentBinding
 import com.malygos.gnemes.utils.InternetUtils
-import timber.log.Timber
 
 
 class NewPostFragment : Fragment() {
 
     private lateinit var viewModel: NewPostViewModel
-    private lateinit var binding:NewPostFragmentBinding
+    private lateinit var binding: NewPostFragmentBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = NewPostFragmentBinding.inflate(inflater, container, false)
-        if(InternetUtils.hasNetworkAvailable(context)){
+        if (InternetUtils.hasNetworkAvailable(context)) {
             val repository = MemePostRepository(GnemesApiService.invoke())
-            viewModel = ViewModelProviders.of(this,NewPostViewModelFactory(repository)).get(NewPostViewModel::class.java)
-            if(viewModel.memePost.value==null){
+            viewModel = ViewModelProviders.of(this, NewPostViewModelFactory(repository))
+                .get(NewPostViewModel::class.java)
+            if (viewModel.memePost.value == null) {
                 return binding.root
             }
             viewModel.memePost.observe(viewLifecycleOwner, Observer {
-                Log.d("it.isSuccessful!!observe","observe")
-                if(it.isSuccessful){
-                    binding.recyclerMemePost.layoutManager=LinearLayoutManager(context,LinearLayoutManager.VERTICAL,false)
-                    binding.recyclerMemePost.adapter=MemePostsAdapter(it.body!!)
+                if (it.isSuccessful) {
+                    binding.recyclerMemePost.layoutManager =
+                        LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+                    binding.recyclerMemePost.adapter = MemePostsAdapter(it.body!!)
                     val decoration = DividerItemDecoration(
                         context,
                         DividerItemDecoration.VERTICAL
                     )
                     decoration.setDrawable(requireContext().getDrawable(R.drawable.divider_recyclerview)!!)
                     binding.recyclerMemePost.addItemDecoration(decoration)
-                }else{
-                    Log.d("it.isSuccessful!!",it.errorMessage)
+                } else {
+                    binding.viewOffline.visibility=View.VISIBLE
+                    binding.recyclerMemePost.visibility=View.GONE
                 }
-                }
-          )
-            return binding.root
-        }else{
-            return inflater.inflate(R.layout.ofline_layout_fragment, container, false)
+            }
+            )
+        } else {
+            binding.viewOffline.visibility=View.VISIBLE
+            binding.recyclerMemePost.visibility=View.GONE
         }
+        return binding.root
     }
 
 }
