8538cd1c955a1c643f76b2c408297adbda0f09c9
niconicocbf <niconicocbf@icloud.com>
2020-06-06 15:07:54 +0900

    implement design

--- GnemesAndroid/app/src/main/java/com/malygos/gnemes/ui/fragment/post/MemePostsAdapter.kt
+++ GnemesAndroid/app/src/main/java/com/malygos/gnemes/ui/fragment/post/MemePostsAdapter.kt
@@ -7,15 +7,20 @@ import androidx.recyclerview.widget.RecyclerView
 import com.malygos.gnemes.R
 import com.malygos.gnemes.data.entity.MemePost
 import com.malygos.gnemes.databinding.ListItemMemePostBinding
+import com.malygos.gnemes.ui.MainActivity
+import com.malygos.gnemes.ui.fragment.detail.MemeDetailFragment
 
 class MemePostsAdapter(
     val memePosts: List<MemePost>
 ) : RecyclerView.Adapter<MemePostsAdapter.MemePostHolder>() {
+    private lateinit var parent:ViewGroup
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): MemePostsAdapter.MemePostHolder {
+        this.parent =parent
+
         return MemePostHolder(
             DataBindingUtil.inflate(
                 LayoutInflater.from(parent.context),
@@ -32,6 +37,11 @@ class MemePostsAdapter(
 
     override fun onBindViewHolder(holder: MemePostsAdapter.MemePostHolder, position: Int) {
         holder.binding.memePost = memePosts[position]
+        holder.binding.memeImg.setOnClickListener {
+            val newInstance = MemeDetailFragment.newInstance("1", "2")
+            val ft = (parent.context as MainActivity).supportFragmentManager
+            newInstance.show(ft,"test")
+        }
     }
 
     inner class MemePostHolder(val binding: ListItemMemePostBinding) :
