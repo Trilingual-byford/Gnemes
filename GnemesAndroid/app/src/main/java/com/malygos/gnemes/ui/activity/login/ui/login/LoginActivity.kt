d848a2bc8c1b7240386e1004621b27418d374265
niconicocbf <niconicocbf@icloud.com>
2020-09-19 23:45:11 +0900

    implement navigation graph for login logic

--- GnemesAndroid/app/src/main/java/com/malygos/gnemes/ui/activity/login/ui/login/LoginActivity.kt
+++ GnemesAndroid/app/src/main/java/com/malygos/gnemes/ui/activity/login/ui/login/LoginActivity.kt
@@ -37,101 +37,92 @@ class LoginActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_login)
 
-        val username = findViewById<EditText>(R.id.username)
-        val password = findViewById<EditText>(R.id.password)
-        val login = findViewById<Button>(R.id.login)
-        val loading = findViewById<ProgressBar>(R.id.loading)
-
-        loginViewModel = ViewModelProviders.of(this, LoginViewModelFactory())
-            .get(LoginViewModel::class.java)
-
-        loginViewModel.loginFormState.observe(this@LoginActivity, Observer {
-            val loginState = it ?: return@Observer
-
-            // disable login button unless both username / password is valid
-            login.isEnabled = loginState.isDataValid
-
-            if (loginState.usernameError != null) {
-                username.error = getString(loginState.usernameError)
-            }
-            if (loginState.passwordError != null) {
-                password.error = getString(loginState.passwordError)
-            }
-        })
-
-        loginViewModel.loginResult.observe(this@LoginActivity, Observer {
-            val loginResult = it ?: return@Observer
-
-            loading.visibility = View.GONE
-            if (loginResult.error != null) {
-                showLoginFailed(loginResult.error)
-            }
-            if (loginResult.success != null) {
-                updateUiWithUser(loginResult.success)
-            }
-            setResult(Activity.RESULT_OK)
-
-            //Complete and destroy login activity once successful
-            finish()
-        })
-
-        username.afterTextChanged {
-            loginViewModel.loginDataChanged(
-                username.text.toString(),
-                password.text.toString()
-            )
-        }
-
-        password.apply {
-            afterTextChanged {
-                loginViewModel.loginDataChanged(
-                    username.text.toString(),
-                    password.text.toString()
-                )
-            }
-
-            setOnEditorActionListener { _, actionId, _ ->
-                when (actionId) {
-                    EditorInfo.IME_ACTION_DONE ->
-                        loginViewModel.login(
-                            username.text.toString(),
-                            password.text.toString()
-                        )
-                }
-                false
-            }
-
-            login.setOnClickListener {
-                loading.visibility = View.VISIBLE
-//                loginViewModel.login(username.text.toString(), password.text.toString())
-
-            }
-        }
+//        val username = findViewById<EditText>(R.id.username)
+//        val password = findViewById<EditText>(R.id.password)
+//        val login = findViewById<Button>(R.id.login)
+//        val loading = findViewById<ProgressBar>(R.id.loading)
+//
+//        loginViewModel = ViewModelProviders.of(this, LoginViewModelFactory())
+//            .get(LoginViewModel::class.java)
+//
+//        loginViewModel.loginFormState.observe(this@LoginActivity, Observer {
+//            val loginState = it ?: return@Observer
+//
+//            // disable login button unless both username / password is valid
+//            login.isEnabled = loginState.isDataValid
+//
+//            if (loginState.usernameError != null) {
+//                username.error = getString(loginState.usernameError)
+//            }
+//            if (loginState.passwordError != null) {
+//                password.error = getString(loginState.passwordError)
+//            }
+//        })
+//
+//        loginViewModel.loginResult.observe(this@LoginActivity, Observer {
+//            val loginResult = it ?: return@Observer
+//
+//            loading.visibility = View.GONE
+//            if (loginResult.error != null) {
+//                showLoginFailed(loginResult.error)
+//            }
+//            if (loginResult.success != null) {
+//                updateUiWithUser(loginResult.success)
+//            }
+//            setResult(Activity.RESULT_OK)
+//
+//            //Complete and destroy login activity once successful
+//            finish()
+//        })
+//
+//        username.afterTextChanged {
+//            loginViewModel.loginDataChanged(
+//                username.text.toString(),
+//                password.text.toString()
+//            )
+//        }
+//
+//        password.apply {
+//            afterTextChanged {
+//                loginViewModel.loginDataChanged(
+//                    username.text.toString(),
+//                    password.text.toString()
+//                )
+//            }
+//
+//            setOnEditorActionListener { _, actionId, _ ->
+//                when (actionId) {
+//                    EditorInfo.IME_ACTION_DONE ->
+//                        loginViewModel.login(
+//                            username.text.toString(),
+//                            password.text.toString()
+//                        )
+//                }
+//                false
+//            }
+//
+//            login.setOnClickListener {
+//                loading.visibility = View.VISIBLE
+//
+//            }
+//        }
     }
-//
-//    override fun onBackPressed() {
-//        super.onBackPressed()
-////        val intent= Intent(this, MainActivity::class.java)
-////        this.startActivity(intent)
-////        overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_right)
-////        finish()
-//
+
+
+//    private fun updateUiWithUser(model: LoggedInUserView) {
+//        val welcome = getString(R.string.welcome)
+//        val displayName = model.displayName
+//        // TODO : initiate successful logged in experience
+//        Toast.makeText(
+//            applicationContext,
+//            "$welcome $displayName",
+//            Toast.LENGTH_LONG
+//        ).show()
 //    }
-
-    private fun updateUiWithUser(model: LoggedInUserView) {
-        val welcome = getString(R.string.welcome)
-        val displayName = model.displayName
-        // TODO : initiate successful logged in experience
-        Toast.makeText(
-            applicationContext,
-            "$welcome $displayName",
-            Toast.LENGTH_LONG
-        ).show()
-    }
-
-    private fun showLoginFailed(@StringRes errorString: Int) {
-        Toast.makeText(applicationContext, errorString, Toast.LENGTH_SHORT).show()
-    }
+//
+//    private fun showLoginFailed(@StringRes errorString: Int) {
+//        Toast.makeText(applicationContext, errorString, Toast.LENGTH_SHORT).show()
+//    }
 }
 
 /**
