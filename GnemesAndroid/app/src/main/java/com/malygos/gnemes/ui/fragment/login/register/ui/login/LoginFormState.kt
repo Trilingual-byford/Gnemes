5bf82b4fa6090439e207b3781e7cc466daf073da
niconicocbf <niconicocbf@icloud.com>
2020-09-22 20:39:56 +0900

    add register screen file

---
+++ GnemesAndroid/app/src/main/java/com/malygos/gnemes/ui/fragment/login/register/ui/login/LoginFormState.kt
@@ -0,0 +1,10 @@
+package com.malygos.gnemes.ui.fragment.login.register.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(
+    val usernameError: Int? = null,
+    val passwordError: Int? = null,
+    val isDataValid: Boolean = false
+)
